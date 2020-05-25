c654a627340a307b2052276ca65c5e40ba24a860
Easytolearndemo <65667556+Easytolearndemo@users.noreply.github.com>
2020-05-25 08:55:40 +0530

    Add files via upload

---
+++ 2. data_type.py
@@ -0,0 +1,31 @@
+"""a=20
+print(a)
+print(type(a))"""
+
+"""
+a=25.6
+print(a)
+print(type(a))"""
+
+"""
+a=9+5j
+print(a)
+print(type(a))"""
+
+
+"""li=["skylight 4u",50,4]
+print(li)
+print(type(li))"""
+"""
+s='Skylight 4u'
+print(s)
+print(type(s))"""
+
+""""
+s={"Skylight":"4U","a":40}
+print(s)
+print(type(s))"""
+
+s={"s",60,79}
+print(s)
+print(type(s))
