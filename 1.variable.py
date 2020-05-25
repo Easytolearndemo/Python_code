570c7a800e2d53dbb05770eccddaeeea1550a143
Easytolearndemo <65667556+Easytolearndemo@users.noreply.github.com>
2020-05-25 08:51:24 +0530

    Add files via upload

---
+++ 1.variable.py
@@ -0,0 +1,23 @@
+#print("madhumita")
+"""print("skylight 4u")
+print("madhumita")
+print("susmita")"""
+
+"""d=90
+print(d)
+
+
+c="susmita"
+print(c)
+
+f=18.6
+print(f)"""
+
+
+a,v,b=15,10.30,"Skylighu 4u"
+print("a=",a)
+print("v=",v)
+print("b=",b)
+
+
+
