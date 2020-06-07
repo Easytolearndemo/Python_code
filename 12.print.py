3c2cdf80a96a0478429d550d9f1467377442c41e
Easytolearndemo <65667556+Easytolearndemo@users.noreply.github.com>
2020-06-08 00:46:18 +0530

    Add files via upload

---
+++ 12.print.py
@@ -0,0 +1,25 @@
+"""print("our youtub Skylight 4U")
+print('our youtub Skylight 4U')
+print("Like", "Share", "Subscribe")
+print(50)"""
+
+"""data = ['Skylight 4U',10,20,50,-90]
+print(data)"""
+
+"""print("Our channal", end=' ')
+print("is", end=' ')
+print("Skylight")"""
+
+"""print("Our channal", end='\t')
+print("is", end='\t')
+print("Skylight 4U")"""
+
+"""p = 20
+q = 30
+#print(p, q)
+
+print(p, q, sep=',')"""
+
+name = "susmita"
+age = 22
+print("My Name is ", name, "and My age is", age)
---
+++ 13.input.py
@@ -0,0 +1,17 @@
+"""name=input("Enter your name:")
+age=input("Enter your age:")
+print(type(name))
+print(name)
+print(type(age))
+print(age)"""
+
+
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+ra=float(input("Enter your rate:"))
+print(type(name))
+print(name)
+print(type(age))
+print(age)
+print(type(ra))
+print(ra)
