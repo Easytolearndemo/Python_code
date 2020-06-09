2d65e42bde8450b33f887af4318bc26f4d9b64e9
Easytolearndemo <65667556+Easytolearndemo@users.noreply.github.com>
2020-06-09 11:25:51 +0530

    Add files via upload

---
+++ 14.if_statement.py
@@ -0,0 +1,16 @@
+"""Syntax:
+if (condition):
+     statement1
+     statement2
+Rest of the Code
+"""
+"""if 10<20:
+  print("20 is greater")
+print("our channel is skylight 4U")"""
+
+
+a=10
+b=20
+if a>b:
+  print("20 is greater")
+print("our channel is skylight 4U")
