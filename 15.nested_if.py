b613cb9e217995b558b3901490a1d9e37e328b62
Easytolearndemo <65667556+Easytolearndemo@users.noreply.github.com>
2020-06-10 11:04:57 +0530

    Add files via upload

---
+++ 15.nested_if.py
@@ -0,0 +1,22 @@
+#Nested If Statement
+"""Syntax :
+	if (condition):
+	       block of statements
+	       if(condition):
+	                 block of statements
+	Rest of the code"""
+"""
+if 6>2:
+  print("6 is greater....")
+  if(8<2):
+    print("2 is smallest...")
+print("our channel is skylight 4u")"""
+
+
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+if a>b:
+  print("6 is greater....")
+  if(a<b):
+    print("2 is smallest...")
+print("our channel is skylight 4u")
