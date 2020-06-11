a80f5ca64c86ea4153f2c32780d0e9c3202d1d0d
Easytolearndemo <65667556+Easytolearndemo@users.noreply.github.com>
2020-06-11 10:38:24 +0530

    Add files via upload

---
+++ 16.else_if.py
@@ -0,0 +1,24 @@
+"""#If Else Statement
+Syntex:
+if(condition):
+         Statement 1
+          Statement 2
+else:
+          Statement 3
+          Statement 4
+Rest of the Code"""
+
+"""if 6<3:
+  print("6 is grater!!")
+else:
+  print("3 is smallest!!")
+print("our youtub is skylight 4u")"""
+
+
+a=int(input("enter 1st number:"))
+b=int(input("enter 2nd number:"))
+if a<b:
+  print("6 is grater!!")
+else:
+  print("3 is smallest!!")
+print("our youtub is skylight 4u")
