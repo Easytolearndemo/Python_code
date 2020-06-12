a42abd9825c61bccaf12bd6731459fb7e951e956
Easytolearndemo <65667556+Easytolearndemo@users.noreply.github.com>
2020-06-12 16:57:31 +0530

    Add files via upload

---
+++ 17.nested_if_else.py
@@ -0,0 +1,14 @@
+#Nested If Else Statement
+a = 7
+b = 3
+c = 6
+d = 4
+if a>b:
+	print("a is greater than b")
+	if(c<d):
+		print("c is Greater than d")
+	else:
+		print("d is Greater than c")
+else:
+	print("b is greater than a")
+print("Rest of The Code")
