189439191bedb0d1820786184bfde6976f0064d6
Easytolearndemo <65667556+Easytolearndemo@users.noreply.github.com>
2020-06-13 10:32:47 +0530

    Add files via upload

---
+++ 18.if_elif.py
@@ -0,0 +1,10 @@
+# If elif with User Input
+a = int(input("Enter 1st element:"))
+b = int(input("Enter 2nd element:"))
+if a > b:
+	print("a is greater than b")
+elif a == b:
+	print("a and b are equal")
+elif a < b:
+	print("a is less than b")
+print("our youtub is very helpful!!")
